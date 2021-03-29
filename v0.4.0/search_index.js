d0799c85ee19b1ad5df0e5d33781d13ab7b87383
Documenter.jl <documenter@juliadocs.github.io>
2021-03-29 01:51:37 +0000

    build based on 57068ca

---
+++ v0.4.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = HealthMLBase","category":"page"},{"location":"#HealthMLBase","page":"Home","title":"HealthMLBase","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for HealthMLBase.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [HealthMLBase]","category":"page"}]
+}
